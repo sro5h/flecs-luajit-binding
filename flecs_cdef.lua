fea84262c092730d80d4b418f2dc4e8beac66fad
Paul Meffle <meffle.paul@me.com>
2022-11-26 22:50:16 +0100

    Update submodule `flecs` to commit `caea53a3`

--- flecs_cdef.lua
+++ flecs_cdef.lua
@@ -27,16 +27,28 @@ typedef struct ecs_ref_t ecs_ref_t;
 typedef void (*ecs_iter_action_t)(ecs_iter_t*);
 typedef void (*ecs_iter_init_action_t)(ecs_world_t const*, ecs_poly_t const*, ecs_iter_t*, ecs_term_t*);
 typedef bool (*ecs_iter_next_action_t)(ecs_iter_t*);
-typedef void (*ecs_iter_fini_action_t)(ecs_iter_t *);
+typedef void (*ecs_iter_fini_action_t)(ecs_iter_t*);
 typedef int (*ecs_order_by_action_t)(ecs_entity_t, void const*, ecs_entity_t, void const*);
 typedef void (*ecs_sort_table_action_t)(ecs_world_t*, ecs_table_t*, ecs_entity_t*, void*, int32_t, int32_t, int32_t, ecs_order_by_action_t);
-typedef uint64_t (*ecs_group_by_action_t)(ecs_world_t *, ecs_table_t *, ecs_id_t, void *);
-typedef void* (*ecs_group_create_action_t)(ecs_world_t *, uint64_t, void *);
-typedef void (*ecs_group_delete_action_t)(ecs_world_t *, uint64_t, void *, void*);
+typedef uint64_t (*ecs_group_by_action_t)(ecs_world_t*, ecs_table_t*, ecs_id_t, void*);
+typedef void* (*ecs_group_create_action_t)(ecs_world_t*, uint64_t, void*);
+typedef void (*ecs_group_delete_action_t)(ecs_world_t*, uint64_t, void*, void*);
 typedef void (*ecs_ctx_free_t)(void*);
 typedef void (*ecs_xtor_t)(void*, int32_t, ecs_type_info_t const*);
 typedef void (*ecs_copy_t)(void*, void const*, int32_t, ecs_type_info_t const*);
 typedef void (*ecs_move_t)(void*, void*, int32_t, ecs_type_info_t const*);
+
+typedef struct ecs_entity_desc_t {
+    int32_t _canary;
+    ecs_entity_t id;
+    char const* name;
+    char const* sep;
+    char const* root_sep;
+    char const* symbol;
+    bool use_low_id;
+    ecs_id_t add[32]; // ECS_ID_CACHE_SIZE
+    char const* add_expr;
+} ecs_entity_desc_t;
 
 typedef struct ecs_type_hooks_t {
     ecs_xtor_t ctor;
@@ -50,30 +62,18 @@ typedef struct ecs_type_hooks_t {
     ecs_iter_action_t on_add;
     ecs_iter_action_t on_set;
     ecs_iter_action_t on_remove;
-    void *ctx;
-    void *binding_ctx;
+    void* ctx;
+    void* binding_ctx;
     ecs_ctx_free_t ctx_free;
     ecs_ctx_free_t binding_ctx_free;
 } ecs_type_hooks_t;
-
-typedef struct ecs_entity_desc_t {
-    int32_t _canary;
-    ecs_entity_t id;
-    const char *name;
-    const char *sep;
-    const char *root_sep;
-    const char *symbol;
-    bool use_low_id;
-    ecs_id_t add[32]; // ECS_ID_CACHE_SIZE
-    const char *add_expr;
-} ecs_entity_desc_t;
 
 struct ecs_type_info_t {
     ecs_size_t size;
     ecs_size_t alignment;
     ecs_type_hooks_t hooks;
     ecs_entity_t component;
-    const char *name;
+    char const* name;
 };
 
 typedef struct ecs_component_desc_t {
@@ -84,7 +84,7 @@ typedef struct ecs_component_desc_t {
 
 typedef struct ecs_term_id_t {
     ecs_entity_t id;
-    char *name;
+    char* name;
     ecs_entity_t trav;
     ecs_flags32_t flags;
 } ecs_term_id_t;
@@ -115,45 +115,45 @@ typedef struct ecs_term_t {
     ecs_inout_kind_t inout;
     ecs_oper_kind_t oper;
     ecs_id_t id_flags;
-    char *name;
+    char* name;
     int32_t field_index;
-    ecs_id_record_t *idr;
+    ecs_id_record_t* idr;
     bool move;
 } ecs_term_t;
 
 typedef struct ecs_header_t {
     int32_t magic;
     int32_t type;
-    ecs_mixins_t *mixins;
+    ecs_mixins_t* mixins;
 } ecs_header_t;
 
 typedef struct ecs_iterable_t {
     ecs_iter_init_action_t init;
 } ecs_iterable_t;
 
-struct ecs_filter_t {
+typedef struct ecs_filter_t {
     ecs_header_t hdr;
-    ecs_term_t *terms;
+    ecs_term_t* terms;
     int32_t term_count;
     int32_t field_count;
     bool owned;
     bool terms_owned;
     ecs_flags32_t flags;
-    char *name;
-    char *variable_names[1];
+    char* name;
+    char* variable_names[1];
     ecs_iterable_t iterable;
-};
+} ecs_filter_t;
 
 typedef struct ecs_filter_desc_t {
     int32_t _canary;
     ecs_term_t terms[16]; // ECS_TERM_DESC_CACHE_SIZE
-    ecs_term_t *terms_buffer;
+    ecs_term_t* terms_buffer;
     int32_t terms_buffer_count;
-    ecs_filter_t *storage;
+    ecs_filter_t* storage;
     bool instanced;
     ecs_flags32_t flags;
-    const char *expr;
-    const char *name;
+    char const* expr;
+    char const* name;
 } ecs_filter_desc_t;
 
 typedef struct ecs_query_desc_t {
@@ -166,13 +166,14 @@ typedef struct ecs_query_desc_t {
     ecs_group_by_action_t group_by;
     ecs_group_create_action_t on_group_create;
     ecs_group_delete_action_t on_group_delete;
-    void *group_by_ctx;
+    void* group_by_ctx;
     ecs_ctx_free_t group_by_ctx_free;
-    ecs_query_t *parent;
+    ecs_query_t* parent;
+    ecs_entity_t entity;
 } ecs_query_desc_t;
 
 typedef struct ecs_stack_cursor_t {
-    struct ecs_stack_page_t *cur;
+    struct ecs_stack_page_t* cur;
     int16_t sp;
 } ecs_stack_cursor_t;
 
@@ -189,13 +190,13 @@ typedef struct ecs_table_cache_iter_t {
 
 typedef struct ecs_term_iter_t {
     ecs_term_t term;
-    ecs_id_record_t *self_index;
-    ecs_id_record_t *set_index;
-    ecs_id_record_t *cur;
+    ecs_id_record_t* self_index;
+    ecs_id_record_t* set_index;
+    ecs_id_record_t* cur;
     ecs_table_cache_iter_t it;
     int32_t index;
     int32_t observed_table_count;
-    ecs_table_t *table;
+    ecs_table_t* table;
     int32_t cur_match;
     int32_t match_count;
     int32_t last_column;
@@ -204,7 +205,7 @@ typedef struct ecs_term_iter_t {
     int32_t column;
     ecs_entity_t subject;
     ecs_size_t size;
-    void *ptr;
+    void* ptr;
 } ecs_term_iter_t;
 
 typedef enum ecs_iter_kind_t {
@@ -215,7 +216,7 @@ typedef enum ecs_iter_kind_t {
 } ecs_iter_kind_t;
 
 typedef struct ecs_filter_iter_t {
-    const ecs_filter_t *filter;
+    ecs_filter_t const* filter;
     ecs_iter_kind_t kind;
     ecs_term_iter_t term_iter;
     int32_t matches_left;
@@ -223,7 +224,7 @@ typedef struct ecs_filter_iter_t {
 } ecs_filter_iter_t;
 
 typedef struct ecs_query_iter_t {
-    ecs_query_t *query;
+    ecs_query_t* query;
     ecs_query_table_node_t *node, *prev, *last;
     int32_t sparse_smallest;
     int32_t sparse_first;
@@ -232,10 +233,10 @@ typedef struct ecs_query_iter_t {
 } ecs_query_iter_t;
 
 typedef struct ecs_rule_iter_t {
-    const ecs_rule_t *rule;
-    struct ecs_var_t *registers;
-    struct ecs_rule_op_ctx_t *op_ctx;
-    int32_t *columns;
+    ecs_rule_t const* rule;
+    struct ecs_var_t* registers;
+    struct ecs_rule_op_ctx_t* op_ctx;
+    int32_t* columns;
     ecs_entity_t entity;
     bool redo;
     int32_t op;
@@ -244,7 +245,7 @@ typedef struct ecs_rule_iter_t {
 
 typedef struct ecs_snapshot_iter_t {
     ecs_filter_t filter;
-    ecs_vector_t *tables;
+    ecs_vector_t* tables;
     int32_t index;
 } ecs_snapshot_iter_t;
 
@@ -343,7 +344,7 @@ char const* ecs_get_symbol(ecs_world_t const*, ecs_entity_t);
 ecs_entity_t ecs_set_name(ecs_world_t*, ecs_entity_t, char const*);
 ecs_entity_t ecs_set_symbol(ecs_world_t*, ecs_entity_t, char const*);
 void ecs_set_alias(ecs_world_t*, ecs_entity_t, char const*);
-bool ecs_has_id(const ecs_world_t*, ecs_entity_t, ecs_id_t);
+bool ecs_has_id(ecs_world_t const*, ecs_entity_t, ecs_id_t);
 ecs_entity_t ecs_get_target(ecs_world_t const*, ecs_entity_t, ecs_entity_t, int32_t);
 ecs_entity_t ecs_get_target_for_id(ecs_world_t const*, ecs_entity_t, ecs_entity_t, ecs_id_t);
 void ecs_enable(ecs_world_t*, ecs_entity_t, bool);
@@ -356,7 +357,7 @@ void* ecs_field_w_size(ecs_iter_t const*, size_t, int32_t);
 bool ecs_field_is_readonly(ecs_iter_t const*, int32_t);
 bool ecs_field_is_writeonly(ecs_iter_t const*, int32_t);
 ecs_id_t ecs_field_id(ecs_iter_t const*, int32_t);
-size_t ecs_field_size(ecs_iter_t*, int32_t);
+size_t ecs_field_size(ecs_iter_t const*, int32_t);
 bool ecs_field_is_self(ecs_iter_t const*, int32_t);
 
 ]]
